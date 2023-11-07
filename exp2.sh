636aa8eacbd68a51390b649f344095d70fe0ce71
Navid Madani <navidmdn74@gmail.com>
2023-11-07 09:33:31 -0500

    experiment files updated with flan-t5 models

--- exp2.sh
+++ exp2.sh
@@ -14,7 +14,8 @@
 echo "submitting job..."
 WANDB_ENTITY=navidmdn WANDB_PROJECT=strategy_follower PYTHONPATH=. python strategy_conditioned_utterance_generation_trainer.py --cache_dir "../hfcache" \
 --train_file original_data/train_ann_extes.json --strategy_file original_data/extes_strategy.json \
---validation_file original_data/valid_ann_extes.json --do_train --model_name_or_path facebook/bart-large --output_dir ./output/exp2 --overwrite_output_dir \
+--validation_file original_data/valid_ann_extes.json --do_train --model_name_or_path google/flan-t5-large --output_dir ./output/exp2-flan --overwrite_output_dir \
 --preprocessing_num_workers 4 --num_train_epochs 30 --evaluation_strategy steps --logging_strategy steps --logging_steps 50 --predict_with_generate \
---eval_steps 300 --per_device_train_batch_size 16 --metric_for_best_model loss --load_best_model_at_end --save_total_limit 1 --save_steps 300
+--eval_steps 300 --per_device_train_batch_size 8 --metric_for_best_model loss --load_best_model_at_end --save_total_limit 1 --save_steps 300 \
+--max_source_length 300
 echo "job finished successfully."
