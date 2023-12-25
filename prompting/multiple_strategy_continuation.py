4f08e33e3f159541595dec3e55eab7a785f5cf05
Navid Madani <navidmdn74@gmail.com>
2023-12-25 16:42:39 -0500

    added attention weights and prompts to the output of prompting models

--- prompting/multiple_strategy_continuation.py
+++ prompting/multiple_strategy_continuation.py
@@ -1,6 +1,6 @@
 import json
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-from typing import List, Dict
+from typing import List, Dict, Tuple
 import fire
 from prompting.llama_prompt import modified_extes_support_strategies
 from prompting.llama_prompt import B_SYS, B_INST, E_INST, E_SYS
@@ -9,6 +9,9 @@ import os
 from tqdm import tqdm
 from accelerate import Accelerator
 import random
+import pickle
+import time
+from prompting.analyze_attention_weights import default_aggregate_attention
 
 template1 = """You are a helpful, precise and accurate emotional support expert.\
 The user has come to you with the following situation: "{situation}". continue the\
@@ -26,24 +29,28 @@ def load_jsonl(path):
 
 
 class ESPromptOutput:
-    def __init__(self, dialog: List[str], situation: str, speakers: List[str], responses: Dict[str, str]):
+    def __init__(self, dialog: List[str], situation: str, speakers: List[str], responses: Dict[str, str],
+                 prompts: Dict[str, str], attentions: Dict[str, Tuple] = None):
         self.dialog = dialog
         self.situation = situation
         self.speakers = speakers
         self.responses = responses
+        self.prompts = prompts
+        self.attentions = attentions
 
     def to_dict(self):
         return {
             "dialog": self.dialog,
             "situation": self.situation,
             "speakers": self.speakers,
-            "responses": self.responses
+            "responses": self.responses,
+            "prompts": self.prompts,
         }
 
     def __repr__(self):
         respones_str = ""
         for strategy, resp in self.responses.items():
-            respones_str += f"{'*'*300}\n\n strategy: {strategy}\n\n response: {resp}\n\n{'*'*300}\n\n"
+            respones_str += f"{'*' * 300}\n\n strategy: {strategy}\n\n response: {resp}\n\n{'*' * 300}\n\n"
         return "_______________________".join([
             f"Situation: {self.situation}",
             f"Dialog: {self.dialog}",
@@ -66,30 +73,31 @@ def convert_to_llama2_chat_format_manually(sys_msg: str, conversations: List[str
     i = 0
     while i < len(conversations):
         ai_msg = conversations[i]
-        human_msg = conversations[i+1]
+        human_msg = conversations[i + 1]
         result += f"{ai_msg} </s><s>{B_INST} {human_msg} {E_INST} "
         i += 2
 
     return result
 
-def convert_to_llama2_chat_partial_conv_format(sys_msg: str, conversations: List[str], tokenizer, n_turns_as_conv=3) -> str:
 
+def convert_to_llama2_chat_partial_conv_format(sys_msg: str, conversations: List[str], tokenizer,
+                                               n_turns_as_conv=3) -> str:
     if n_turns_as_conv % 2 != 1:
         raise ValueError("n_turns_as_conv should be odd number")
 
     conv_messages = []
-    for i in range(max(len(conversations)-n_turns_as_conv, 0), len(conversations)-1, 2):
+    for i in range(max(len(conversations) - n_turns_as_conv, 0), len(conversations) - 1, 2):
         conv_messages.append({'role': 'user', 'content': conversations[i].strip()})
-        conv_messages.append({'role': 'assistant', 'content': conversations[i+1].strip()})
+        conv_messages.append({'role': 'assistant', 'content': conversations[i + 1].strip()})
     conv_messages.append({'role': 'user', 'content': conversations[-1].strip()})
 
     if len(conversations) > n_turns_as_conv:
         conversations = conversations[:-n_turns_as_conv]
 
     conv_history_str = "conversation history:\n"
-    for i in range(0, len(conversations)-1, 2):
+    for i in range(0, len(conversations) - 1, 2):
         conv_history_str += "user: " + conversations[i].strip() + "\n"
-        conv_history_str += "assistant: " + conversations[i+1].strip() + "\n"
+        conv_history_str += "assistant: " + conversations[i + 1].strip() + "\n"
 
     sys_msg = f"{conv_history_str}\n{sys_msg.strip()}"
     messages = [{'role': 'system', 'content': sys_msg}]
@@ -98,22 +106,24 @@ def convert_to_llama2_chat_partial_conv_format(sys_msg: str, conversations: List
     formatted_prompt = tokenizer.apply_chat_template(messages, tokenize=False)
     return formatted_prompt
 
+
 def convert_to_llama2_chat_format(sys_msg: str, conversations: List[str], tokenizer) -> str:
     messages = [{'role': 'system', 'content': sys_msg}]
-    for i in range(0, len(conversations)-1, 2):
+    for i in range(0, len(conversations) - 1, 2):
         messages.append({'role': 'user', 'content': conversations[i]})
-        messages.append({'role': 'assistant', 'content': conversations[i+1]})
+        messages.append({'role': 'assistant', 'content': conversations[i + 1]})
     messages.append({'role': 'user', 'content': conversations[-1]})
 
     formatted_prompt = tokenizer.apply_chat_template(messages, tokenize=False)
     return formatted_prompt
 
+
 def convert_to_llama2_llm_format(sys_msg: str, conversations: List[str], tokenizer) -> str:
     formatted_prompt = f"{tokenizer.bos_token}{sys_msg}\n\n"
 
-    for i in range(0, len(conversations)-1, 2):
+    for i in range(0, len(conversations) - 1, 2):
         formatted_prompt += f"seeker: {conversations[i].strip()}\n"
-        formatted_prompt += f"supporter: {conversations[i+1].strip()}\n"
+        formatted_prompt += f"supporter: {conversations[i + 1].strip()}\n"
     formatted_prompt += f"seeker: {conversations[-1].strip()}\n"
     formatted_prompt += f"supporter: "
     return formatted_prompt
@@ -151,7 +161,8 @@ def get_model_and_tokenizer(model_name, cache_dir, load_in_4bit=True):
     return model, tokenizer
 
 
-def get_continuation_prompt(conversation, model, tokenizer, model_type='llama'):
+def get_continuation_prompt(conversation, model, tokenizer, model_type='llama', get_attentions=False,
+                            max_new_tokens=512):
     dialog = conversation['dialog_history']
     speakers = conversation['prev_speakers']
     situation = conversation['situation']
@@ -163,6 +174,8 @@ def get_continuation_prompt(conversation, model, tokenizer, model_type='llama'):
     assert speakers[0] == 'seeker'
     assert speakers[-1] == 'seeker'
     responses = {}
+    prompts = {}
+    attentions = {}
 
     for strategy, desc in tqdm(modified_extes_support_strategies.items()):
         if random.random() > 0.3:
@@ -171,30 +184,45 @@ def get_continuation_prompt(conversation, model, tokenizer, model_type='llama'):
 
         if model_type == 'llama' or model_type == 'mistral':
             prompt = convert_to_llama2_chat_partial_conv_format(sys_msg, dialog, tokenizer)
-
             input_ids = tokenizer(prompt, return_tensors='pt', add_special_tokens=False)['input_ids'].to(model.device)
             print("length of input_ids: ", len(input_ids[0]))
             print("prompt: ", prompt)
+
         elif model_type == 'mistral':
             pass
         else:
             raise ValueError(f"model_type should be one of ['llama', 'mistral'], but got {model_type}")
 
-        outputs = model.generate(input_ids, max_new_tokens=512, num_beams=5, num_return_sequences=1,
-                                 do_sample=True, top_p=0.95, top_k=50, temperature=0.8, repetition_penalty=1.1,
-                                 length_penalty=1.2)
+        # todo: implement aggregation for beam search output
+        # outputs = model.generate(input_ids, output_attentions=get_attentions, max_new_tokens=max_new_tokens, num_beams=5,
+        #                          num_return_sequences=1, do_sample=True, top_p=0.95, top_k=50, temperature=0.8,
+        #                          repetition_penalty=1.1, length_penalty=1.2, return_dict_in_generate=True)
 
-        response = outputs[0][len(input_ids[0]):]
+        outputs = model.generate(input_ids, output_attentions=get_attentions, max_new_tokens=max_new_tokens,
+                                 return_dict_in_generate=True)
+
+        prompts[strategy] = prompt
+        if get_attentions:
+            agg_attentions = [default_aggregate_attention(t).cpu() for t in outputs.attentions]
+            # last tokens attentions should be the size of full sequence
+            assert agg_attentions[-1].shape[0] == len(
+                outputs.sequences[0]), f"{agg_attentions[-1].shape[0]} != {len(outputs.sequences[0])}"
+            attentions[strategy] = (outputs.sequences[0].cpu(), agg_attentions)
+
+        response = outputs.sequences[0][len(input_ids[0]):]
         output_txt = tokenizer.decode(response, skip_special_tokens=True).strip()
         responses[strategy] = output_txt
         print("\n\nresponse: ", output_txt)
 
-    res = ESPromptOutput(dialog=dialog, situation=situation, speakers=speakers, responses=responses)
+    if attentions:
+        return ESPromptOutput(dialog=dialog, situation=situation, speakers=speakers, responses=responses,
+                              prompts=prompts, attentions=attentions)
+    res = ESPromptOutput(dialog=dialog, situation=situation, speakers=speakers, responses=responses, prompts=prompts)
     return res
 
-def run(data_path='../original_data/train.json', min_turn=3, max_turn=12, model_path='meta-llama/Llama-2-7b-chat-hf',
-        cache_dir=None, output_path='./outputs', load_in_4bit=True):
 
+def run(data_path='../original_data/train.json', min_turn=3, max_turn=12, model_path='nickypro/tinyllama-15M',
+        cache_dir=None, output_path='./outputs', load_in_4bit=True, get_attentions=False, max_new_tokens=512):
     data = load_jsonl(data_path)
     data = [d for d in data if min_turn <= d['turn'] <= max_turn]
     model, tokenizer = get_model_and_tokenizer(model_path, cache_dir, load_in_4bit)
@@ -209,10 +237,15 @@ def run(data_path='../original_data/train.json', min_turn=3, max_turn=12, model_
             continue
 
         conversation = data[rand_id]
-        generated_conts = get_continuation_prompt(conversation, model, tokenizer)
+        generated_conts = get_continuation_prompt(conversation, model, tokenizer, get_attentions=get_attentions,
+                                                  max_new_tokens=max_new_tokens)
 
         with open(os.path.join(output_path, f'{rand_id}.json'), 'w') as f:
             json.dump(generated_conts.to_dict(), f)
+
+        if get_attentions:
+            with open(os.path.join(output_path, f'{rand_id}_attentions.pkl'), 'wb') as f:
+                pickle.dump(generated_conts.attentions, f)
 
 
 if __name__ == '__main__':
